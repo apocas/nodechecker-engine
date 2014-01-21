153c9bf61fd8ef0bdd220afca53ab6d5062fe63a
apocas <petermdias@gmail.com>
2014-01-21 01:51:29 +0000

    revamp stable

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var cp = require('child_process');
 var childs = [];
 var procs = process.env.PROCS || 6;
 
-for (var i = procs; i >= 0; i--) {
+for (var i = procs; i > 0; i--) {
   childs.push(cp.fork('./work'));
 }
